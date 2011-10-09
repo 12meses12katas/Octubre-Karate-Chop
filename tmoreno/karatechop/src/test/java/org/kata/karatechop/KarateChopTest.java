70b80d69ad931dbe21ebcfa629db837640005436
tmoreno <tmorenobernal@yahoo.es>
2011-10-09 18:45:47 +0200

    Version iterativa

--- tmoreno/karatechop/src/test/java/org/kata/karatechop/KarateChopTest.java
+++ tmoreno/karatechop/src/test/java/org/kata/karatechop/KarateChopTest.java
@@ -57,4 +57,45 @@ public class KarateChopTest {
 		assertEquals(-1, KarateChopRecursivo.chop(12, intArray6));
 		assertEquals(-1, KarateChopRecursivo.chop(14, intArray6));
 	}
+	
+	@Test
+	public void karateChopIterativoTest() {
+		assertEquals(-1, KarateChopIterativo.chop(3, emptyArray));
+		assertEquals(-1, KarateChopIterativo.chop(3, intArray1));
+		assertEquals(0, KarateChopIterativo.chop(1, intArray1));
+		
+		assertEquals(0, KarateChopIterativo.chop(1, intArray3));
+		assertEquals(1, KarateChopIterativo.chop(3, intArray3));
+		assertEquals(2, KarateChopIterativo.chop(5, intArray3));
+		assertEquals(-1, KarateChopIterativo.chop(0, intArray3));
+		assertEquals(-1, KarateChopIterativo.chop(2, intArray3));
+		assertEquals(-1, KarateChopIterativo.chop(4, intArray3));
+		assertEquals(-1, KarateChopIterativo.chop(6, intArray3));
+		
+		assertEquals(0, KarateChopIterativo.chop(1, intArray4));
+		assertEquals(1, KarateChopIterativo.chop(3, intArray4));
+		assertEquals(2, KarateChopIterativo.chop(5, intArray4));
+		assertEquals(3, KarateChopIterativo.chop(7, intArray4));
+		assertEquals(-1, KarateChopIterativo.chop(0, intArray4));
+		assertEquals(-1, KarateChopIterativo.chop(2, intArray4));
+		assertEquals(-1, KarateChopIterativo.chop(4, intArray4));
+		assertEquals(-1, KarateChopIterativo.chop(6, intArray4));
+		assertEquals(-1, KarateChopIterativo.chop(8, intArray4));
+		
+		assertEquals(0, KarateChopIterativo.chop(1, intArray6));
+		assertEquals(1, KarateChopIterativo.chop(3, intArray6));
+		assertEquals(2, KarateChopIterativo.chop(5, intArray6));
+		assertEquals(3, KarateChopIterativo.chop(7, intArray6));
+		assertEquals(4, KarateChopIterativo.chop(9, intArray6));
+		assertEquals(5, KarateChopIterativo.chop(11, intArray6));
+		assertEquals(6, KarateChopIterativo.chop(13, intArray6));
+		assertEquals(-1, KarateChopIterativo.chop(0, intArray6));
+		assertEquals(-1, KarateChopIterativo.chop(2, intArray6));
+		assertEquals(-1, KarateChopIterativo.chop(4, intArray6));
+		assertEquals(-1, KarateChopIterativo.chop(6, intArray6));
+		assertEquals(-1, KarateChopIterativo.chop(8, intArray6));
+		assertEquals(-1, KarateChopIterativo.chop(10, intArray6));
+		assertEquals(-1, KarateChopIterativo.chop(12, intArray6));
+		assertEquals(-1, KarateChopIterativo.chop(14, intArray6));
+	}
 }
