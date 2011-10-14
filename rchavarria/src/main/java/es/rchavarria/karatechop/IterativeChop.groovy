cd4b15090258e0307e04aacf9fcecf8b50348387
txingo <myemail@txingo.server>
2011-10-14 22:25:46 +0200

    Chop iterativo. Primeros tests. La lista de numeros es una lista vacia

---
+++ rchavarria/src/main/java/es/rchavarria/karatechop/IterativeChop.groovy
@@ -0,0 +1,8 @@
+package es.rchavarria.karatechop
+
+class IterativeChop {
+
+    public chop(def target, def values){
+        -1
+    }
+}
---
+++ rchavarria/src/test/java/es/rchavarria/karatechop/KarateChopTest.groovy
@@ -0,0 +1,41 @@
+package es.rchavarria.karatechop;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Ignore 
+import org.junit.Test;
+
+class KarateChopTest {
+
+    private karate
+    
+    @Before
+    public void setUp() throws Exception {
+        karate = new IterativeChop()
+    }
+
+    @Test
+    void testEmptyArray() {
+        assert -1 == karate.chop(3, [])
+    }
+    
+    @Test
+    @Ignore
+    void testOneElementArray() {
+        assert -1 == karate.chop(3, [1])
+        assert 0 == karate.chop(1, [1])
+    }
+    
+    @Test
+    @Ignore
+    void testThreeElementsArray() {
+        assert 0 == karate.chop(1, [1, 3, 5])
+        assert 1 == karate.chop(3, [1, 3, 5])
+        assert 2 == karate.chop(5, [1, 3, 5])
+        assert -1 == karate.chop(0, [1, 3, 5])
+        assert -1 == karate.chop(2, [1, 3, 5])
+        assert -1 == karate.chop(4, [1, 3, 5])
+        assert -1 == karate.chop(6, [1, 3, 5])
+    }
+}
