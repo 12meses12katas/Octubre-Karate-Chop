49defade9b547384406ab5224fb058cb40056d42
elmendalerenda <elmendalerenda@gmail.com>
2011-11-27 01:04:49 +0100

    firt refactir iterative

--- elmendalerenda/js/Iterative.js
+++ elmendalerenda/js/Iterative.js
@@ -1,37 +1,32 @@
 function Iterative() {
 
+    this.notFound = -1;
+
     this.chop = function(value, collection) {
-        if (collection.length == 0) return -1
-
-        var found = -1;
-
+        var found = this.notFound;
         var first = 0;
         var last = collection.length - 1;
-        var middle = Math.floor(last / 2);
+        var middle = -1;
 
         while (last >= first) {
-
-            if (collection[middle] == value) {
-                return middle;
+            middle = Math.floor((first + last) / 2);
+	    var elementFound = (collection[middle] == value);
+            if (elementFound) {
                 found = middle;
                 break;
             }
 
-            var inLeftSide = collection[middle] > value
-            if (inLeftSide) {
+            var inLeftSide = collection[middle] > value;
+	    var inRightSide = collection[middle] < value;
+	    
+	    if (inLeftSide) {
                 last = middle - 1;
-            } else {
-                var inRightSide = collection[middle] < value
-
-                if (inRightSide) {
-                    first = middle + 1;
-                }
             }
-
-            middle = Math.floor((first + last) / 2);
-
+	    
+	    if (inRightSide){
+                first = middle + 1;
+            }
         }
-
         return found;
     }
 }
